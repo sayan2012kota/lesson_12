5e5869c1898f228c2baac43cae5801b0c45919a5
sayan2012kota <sayankota2012@gmail.com>
2025-05-31 13:51:49 +0100

    Add files via upload

--- Galaga_game.py
+++ Galaga_game.py
@@ -7,6 +7,12 @@ HEIGHT = 600
 bug = Actor("bug")
 ship = Actor("ship")
 speed = 5
+bugs = []
+bugs.append(bug)
+bugs[-1].x = 900
+bugs[-1].y = -100
+bullets = []
+score = 0
 
 ship.pos = (500, 500)
 
@@ -14,9 +20,15 @@ def draw():
     screen.clear()
     screen.fill("dark blue")
     ship.draw()
+    for V in bugs:
+        V.draw()
+    for R in bullets:
+        R.draw()
+    display_score()
 
 
 def update():
+    global score
     if keyboard.left:
         ship.x = ship.x - speed
         if ship.x <= 0:
@@ -26,6 +38,31 @@ def update():
         ship.x = ship.x + speed
         if ship.x >= WIDTH:
             ship.x = WIDTH    
+    for V in bugs:
+        V.y = V.y + 5
+        if V.y > 600:
+            V.y = -100
+            V.x = random.randint(100, 900)
+    for R in bullets:
+        if R.y < 0:
+            bullets.remove(R)
+        else:
+            R.y = R.y - 5
+    for Bullet in bullets:
+        if bug.colliderect(Bullet):
+            bullets.remove(Bullet)
+            bugs.remove(bug)
+            score = score + 1
+
+
+def display_score():
+    screen.draw.text(str(score), (50, 50))
+
+def on_key_down(key):
+    if key == keys.SPACE:
+        bullets.append(Actor("bullet"))
+        bullets[-1].x = ship.x
+        bullets[-1].y = ship.y - 50
 
 
 
