54da627a1cfcb02988f7f67cc28d8bb95126c887
sayan2012kota <sayankota2012@gmail.com>
2025-06-22 09:17:41 +0100

    Add files via upload

--- hw.py
+++ hw.py
@@ -11,29 +11,34 @@ bugs = []
 bullets = []
 direction = 1
 score = 0
-
+ship.dead = False
+ship.countdown = 80
 ship.pos = (500, 500)
 
 for I in range(8):
-    bugs.append(Actor("shark_image"))
-    bugs[-1].x = 100 + 80 * I
-    bugs[-1].y = 0
-
-
+    for F in range(4):
+        bugs.append(Actor("bug"))
+        bugs[-1].x = 100 + 80 * I
+        bugs[-1].y = 60 + 80 * F
 
 
 def draw():
     screen.clear()
     screen.fill("light blue")
-    ship.draw()
     for V in bugs:
         V.draw()
     for R in bullets:
         R.draw()
     display_score()
+    if len(bugs) == 0:
+        game_over()
+    if ship.dead == False:
+        ship.draw()
 
 
 def update():
+    global direction
+    movedown = False
     global score
     if keyboard.left:
         ship.x = ship.x - speed
@@ -44,26 +49,30 @@ def update():
         ship.x = ship.x + speed
         if ship.x >= WIDTH:
             ship.x = WIDTH    
+    if len(bugs) > 0 and (bugs[-1].x > WIDTH - 80 or bugs[0].x  < 0):
+        movedown = True
+        direction = direction * -1
     for V in bugs:
-        V.y = V.y + 5
+        if V.colliderect(ship):
+            ship.dead = True
+        V.x = V.x + 5 * direction
+        if movedown == True:
+            V.y = V.y + 5
         if V.y > 600:
-            V.y = -100
-            V.x = random.randint(100, 900)
+            bugs.remove(V)
+        for Bullet in bullets:
+            if V.colliderect(Bullet):
+                bullets.remove(Bullet)
+                bugs.remove(V)
+                score = score + 1
     for R in bullets:
         if R.y < 0:
             bullets.remove(R)
         else:
             R.y = R.y - 5
-    for Bullet in bullets:
-        if bug.colliderect(Bullet):
-            bullets.remove(Bullet)
-            bugs.remove(bug)
-            score = score + 1
-    for R in bullets:
-        if R.y < 0:
-            bullets.remove(R)
-        else:
-            R.y = R.y - 5
+
+def game_over():
+    screen.draw.text("Game over", (500,300))
 
 
 def display_score():
